eef220c456f9d7ae8bc7f4ffb8e889af5be1d9d1
Robert Leach <rleach@genomics.princeton.edu>
2014-06-02 17:07:00 -0400

    Updated version number.

--- src/getCandidates.pl
+++ src/getCandidates.pl
@@ -12,7 +12,7 @@
 #Copyright 2014
 
 #These variables (in main) are used by getVersion() and usage()
-my $software_version_number = '1.11';
+my $software_version_number = '1.12';
 my $created_on_date         = '3/24/2014';
 
 ##
